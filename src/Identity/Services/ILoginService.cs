85634100b68b015c3d6ea3454fd6cb917af77826
akhadov <shakhriyor.akhadov@gmail.com>
2024-02-06 20:54:53 +0500

    addde identity services

---
+++ src/Identity/Services/ILoginService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace Identity.Services;
+
+public interface ILoginService<T>
+{
+    Task<bool> ValidateCredentials(T user, string password);
+
+    Task<T> FindByUsername(string user);
+
+    Task SignIn(T user);
+
+    Task SignInAsync(T user, AuthenticationProperties properties, string authenticationMethod = null);
+}
