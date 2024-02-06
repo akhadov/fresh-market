720394e8017b76acc26ce3ed32d00cbec1a0fda3
akhadov <shakhriyor.akhadov@gmail.com>
2024-02-06 23:40:12 +0500

    some changes

---
+++ src/WebApi/Controllers/Account/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers.Account
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+    }
+}
