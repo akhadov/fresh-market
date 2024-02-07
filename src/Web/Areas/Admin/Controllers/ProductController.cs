6d674ccf4b1b57ca79e3af799ab883133da56ba2
akhadov <shakhriyor.akhadov@gmail.com>
2024-02-07 12:58:47 +0500

    added product controller

---
+++ src/Web/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+
+//namespace Web.Areas.Admin.Controllers
+//{
+//    public class ProductController : Controller
+//    {
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
+//    }
+//}
